5258c72a34ddf166435ada331aaeb71c9d705f72
Pedro Dominguez Aguilera <p.dominguezaguilera@ext.douglas.eu>
2023-07-05 16:02:25 +0200

    Fix linter

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
+import viteLogo from 'vite.svg';
 import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+
 import './App.css';
 
 function App() {
@@ -18,7 +19,9 @@ function App() {
             </div>
             <h1>Vite + React</h1>
             <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+                <button type="button" onClick={() => setCount((newValue) => newValue + 1)}>
+                    count is {count}
+                </button>
                 <p>
                     Edit <code>src/App.tsx</code> and save to test HMR
                 </p>
