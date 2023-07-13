ec51ec8ef917ea2826579d0682aacde225cebff3
Pistolero3 <antoniojmoralesmorales@gmail.com>
2023-07-14 00:34:05 +0200

    identify the user and create the cookie

---
+++ frontend/src/GetUser/GetUser.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { useEffect } from 'react';
+
+import Cookie from 'js-cookie';
+
+import './GetUser.scss';
+
+export function GetUser(): JSX.Element {
+    const [cookieUser, setCookieUser] = React.useState<string>('');
+
+    useEffect(() => {
+        const cookieValue = Cookie.get('zoom-waiting-room-user');
+        const loggedInSection: HTMLElement = document.getElementById('loggedIn') as HTMLElement;
+        if (loggedInSection) {
+            loggedInSection.innerHTML = cookieValue ? `You are logged in as ${cookieValue}` : 'You need to log in';
+        }
+    }, []);
+
+    const clearInput = (e: React.FocusEvent<HTMLInputElement>) => {
+        setTimeout(() => (e.target.value = ''), 1000);
+    };
+
+    const handleInputChange = (e: React.FocusEvent<HTMLInputElement>) => {
+        setCookieUser(e.target.value);
+    };
+
+    const handleSetCookie = () => {
+        Cookie.set('zoom-waiting-room-user', cookieUser);
+    };
+
+    return (
+        <>
+            <div>
+                <p id="loggedIn"></p>
+            </div>
+            <div className="inputContainer">
+                <input type="text" className="input" placeholder="User" onChange={handleInputChange} onBlur={clearInput} />
+                <button className="buttonSubmit" type="submit" onClick={handleSetCookie}>
+                    Log in
+                </button>
+            </div>
+        </>
+    );
+}
+
+export default GetUser;
