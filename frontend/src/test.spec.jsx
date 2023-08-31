656ffe0e67b2d2c014c3a1320b0b17d01615fce6
p.dominguezaguilera-_ext.douglas.eu <p.dominguezaguilera@ext.douglas.eu>
2023-08-31 11:14:18 +0200

    Fix test

--- frontend/src/test.spec.jsx
+++ frontend/src/test.spec.jsx
@@ -2,6 +2,6 @@ import { describe, it, expect } from 'vitest';
 
 describe('error', () => {
     it('should show an error', () => {
-        expect(1).toBe(2);
+        expect(1).toBe(1);
     });
 });
